de7624767f6c27815ad7b16458abe578a0b7b110
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-09-08 23:10:54 +0300

    upload

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+// require("dotenv").config();
+// const app = require("./app");
+// const connectDB = require("./config/db");
+
+// const PORT = process.env.PORT || 3000;
+
+// connectDB();
+
+// app.listen(PORT, () => {
+//   console.log(`Server Running On Port ${PORT}`);
+// });
+
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 3000;
-
 connectDB();
 
-app.listen(PORT, () => {
-  console.log(`Server Running On Port ${PORT}`);
-});
+module.exports = app;
