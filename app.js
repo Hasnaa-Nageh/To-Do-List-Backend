0f40f7bdde7c894b900a4919b2c28f0788ef6860
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-09-09 18:28:03 +0300

    upload

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
 const cors = require("cors");
 app.use(
   cors({
-    origin: "*",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
