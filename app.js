f0d722fe47b03986b6e50da85b2f2c3c189b1786
Hasnaa-Nageh <hassnaanageh3@gmail.com>
2025-09-09 12:28:38 +0300

    upload

--- app.js
+++ app.js
@@ -11,6 +11,15 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   res.send("Backend API is running");
 });
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use("/api/auth", authRouter);
 app.use("/api/profile", profileRouter);
